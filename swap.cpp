7645da625d904543544830067374eb04149b9f84
Archit Razdan <arazdan@umass.edu>
2019-07-06 17:24:05 -0400

    Initial commit

---
+++ swap.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+swapFunction(int &x, int &y){
+	int z;
+	z = x;
+	x = y;
+	y = z;
+	
+	
+	
+}
+
+int main(){
+	int a = 5;
+	int b = 10;
+	
+	swapFunction(a,b);
+	
+	
+	
+}
